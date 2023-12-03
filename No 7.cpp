a1b1abba04f2c2caa9d9640f5d64d0e9e014ff1a
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-12-03 22:16:02 +0600

    second commit

---
+++ No 7.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    // int k = x % 10;
+    while (1)
+    {
+        if (x == 7)
+        {
+            break;
+        }
+        else
+        {
+            // k--;
+            x--;
+        }
+    }
+    cout << x << endl;
+}
