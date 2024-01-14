22cc381e9e27fe7a7513a6814611286c303fa46a
Tamim khan <mdtamimkhan842@gmail.com>
2024-01-15 00:31:21 +0600

    second commit

---
+++ uva_problem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define MTK                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+typedef long long int ll;
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+int32_t main()
+{
+    MTK;
+    int b, p, m;
+    while (cin >> b >> p >> m)
+    {
+        b = b % m;
+        p = p % m;
+        ll ans = 1LL * pow(1LL * b, 1LL * p) % m;
+        cout << ans << '\n';
+    }
+}
