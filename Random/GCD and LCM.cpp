dcc44b6bd281e94e9adf82f9e21bf8a13f07b217
Tamim khan <mdtamimkhan842@gmail.com>
2024-09-02 11:49:41 +0600

    added gcd and lcm problem through into the random file

---
+++ Random/GCD and LCM.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MTK                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define mem(a, b) memset(a, b, sizeof(a))
+#define trace(x) cout << #x << ' ' << x << endl
+#define all(x) (x).begin(), (x).end()
+#define ll int long long
+#define mod 1000000007
+
+int32_t main()
+{
+    MTK;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << __gcd(a, b) << ' ' << (a * b) / __gcd(a, b) << '\n';
+    }
+    return 0;
+}
